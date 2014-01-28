e5eab1fca76cdba5a2fa3b0575e92e97c720c544
Jen E <jeneisenberg5@gmail.com>
2014-01-28 17:00:44 -0500

    outline there

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,41 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require ../../../vendor/assets/javascripts/bootstrap.js
+//= require ../../../vendor/assets/javascripts/header.js
+//= require ../../../vendor/assets/javascripts/jquery.easing.1.3.js
+//= require ../../../vendor/assets/javascripts/jquery.min.js
+//= require ../../../vendor/assets/javascripts/retina.js
+//= require ../../../vendor/assets/javascripts/smoothscroll.js
+//= require ../../../vendor/assets/javascripts/block_animate.js
+//= require ../../../vendor/assets/javascripts/sorting.js
+//= require ../../../vendor/assets/javascripts/myscript.js
+//= require ../../../vendor/assets/javascripts/jquery.iconmenu.js
+//= require ../../../vendor/assets/javascripts/skills.js
+//= require ../../../vendor/assets/javascripts/waypoints.js
+//= require ../../../vendor/assets/javascripts/jcarousellite_1.0.1.min.js
+//= require ../../../vendor/assets/javascripts/jquery.isotope.min.js
+//= require ../../../vendor/assets/javascripts/jquery.prettyPhoto.js
+//= require ../../../vendor/assets/javascripts/jquery.stellar.min.js
+
+
+
+
+
+
+
+
+
+
+// require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DermatologyEisenberg::Application.config.secret_token = '3b72ada4f9ef65d6902686d877899d94c54460069a94ca49d92a1bfe7bbef8d0331a35de722c21fc3b166ba064e2d9c376ad1b5292357a628b4904f1fedef9d6'
